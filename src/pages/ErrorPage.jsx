07b4b2b7ae07ef770fe9c3252ca47c87ff4ddd77
mahmutunder <mahmutunder@gmail.com>
2025-02-16 14:14:11 -0700

    added image fix

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,6 +2,9 @@ import styles from './ErrorPage.module.css'
 function ErrorPage() {
     return (
         <div className={styles.error}>
+            <div className='image'>
+
+            </div>
            
         </div>
     )
