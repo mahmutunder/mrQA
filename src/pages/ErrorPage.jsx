3492096110c52cb956cfbb096b2c0502d6ddb5bc
mahmutunder <mahmutunder@gmail.com>
2025-02-16 14:05:51 -0700

    added image

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,7 +2,7 @@ import styles from './ErrorPage.module.css'
 function ErrorPage() {
     return (
         <div className={styles.error}>
-            <img src="../error.jpg"  alt="404 Error"/>
+           
         </div>
     )
 }
